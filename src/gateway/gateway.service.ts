0d015670cb8c2496f1617ea6b64df6dd2126e676
Matt Raffel <mattr@kiva.org>
2022-06-01 10:52:35 -0600

    fixes one error
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- src/gateway/gateway.service.ts
+++ src/gateway/gateway.service.ts
@@ -1,16 +1,19 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { ProtocolLogger } from 'protocol-common';
 import { GatewayRoutes } from './gateway.routes.js';
 
 @Injectable()
 export class GatewayService {
 
+    constructor(readonly logger: ProtocolLogger) {
+    }
     /**
      * Nestjs treats the middleware http proxy requests different than regular request so doesn't trigger the interceptors and filters we're used to
      * Need to apply logger to http-proxy itself
      */
     public getDefaultOptions(): object {
         return {
-            logProvider: () => Logger,
+            logProvider: () => this.logger,
             changeOrigin: true,
             prependPath: false,
         };
